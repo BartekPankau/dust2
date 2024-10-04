47ca77f80926723fe885ad02ff40a9a2feb9fddb
Bartek_p <bartek0288@gmail.com>
2024-10-04 13:54:47 +0200

    treningi tsx

--- TreningJeden.tsx
+++ TreningJeden.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import 'reactjs-popup/dist/index.css'
-import supabase from './supabaseClient.js'
+import supabase from '@/supabaseClient.js'
 import { useEffect, useState } from 'react'
+//import './tabela.css'; // Importowanie pliku CSS
 import '@/globals.css';
-import './boxtabela.css';
+import '@/boxtabela.css';
 
 interface TreningJedenProps {
     horseIdT: number; // ID konia
@@ -14,66 +15,66 @@ interface TreningJedenProps {
 
 type TreningData = {
     [key: string]: any; // Typowanie ogólne dla kolumn
-  };
-  
-  const TreningJeden: React.FC<TreningJedenProps> = ({ horseIdT }) => {
+};
+
+const TreningJeden: React.FC<TreningJedenProps> = ({ horseIdT }) => {
     const [columns, setColumns] = useState<string[]>([]);
     const [data, setData] = useState<TreningData[]>([]);
     const [loading, setLoading] = useState(true);
-  
+
     useEffect(() => {
-      const fetchData = async () => {
-        // Pobieranie danych z tabeli 'trening'
-        let { data, error } = await supabase
-          .from('trening')
-          .select('*, imie')
-          .eq('nr_konia',  horseIdT); 
-          
-        
-        if (error) {
-          console.error(error);
-          setLoading(false);
-          return;
-        }
-  
-        if (data && data.length > 0) {
-          // Pobieranie nazw kolumn z pierwszego wiersza danych
-          setColumns(Object.keys(data[0]));
-          setData(data);
-        }
-  
-        setLoading(false);
-      };
-  
-      fetchData();
+        const fetchData = async () => {
+            // Pobieranie danych z tabeli 'trening'
+            let { data, error } = await supabase
+                .from('trening')
+                .select('*, imie')
+                .eq('nr_konia',  horseIdT);
+
+
+            if (error) {
+                console.error(error);
+                setLoading(false);
+                return;
+            }
+
+            if (data && data.length > 0) {
+                // Pobieranie nazw kolumn z pierwszego wiersza danych
+                setColumns(Object.keys(data[0]));
+                setData(data);
+            }
+
+            setLoading(false);
+        };
+
+        fetchData();
     }, [horseIdT]);
-  
+
     if (loading) {
-      return <div>Ładowanie danych...</div>;
+        return <div>Ładowanie danych...</div>;
     }
 
-      return (
-       
-          <table >
-              <thead>
-              <tr>
-                
-                  {columns.map((column) => (
-                      <th key={column} >{column}</th>
-                  ))}
-              </tr>
-              </thead>
-              <tbody>
-              {data.map((row, rowIndex) => (
-                  <tr key={rowIndex}>
-                      {columns.map((column) => (
-                          <td key={column} >{row[column]}</td>
-                      ))}
-                  </tr>
-              ))}
-              </tbody>
-          </table>
-      );
-  };
-  
-  export default TreningJeden;
+    return (
+
+        <table >
+            <thead>
+            <tr>
+
+                {columns.map((column) => (
+                    <th key={column} >{column}</th>
+                ))}
+            </tr>
+            </thead>
+            <tbody>
+            {data.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                    {columns.map((column) => (
+                        <td key={column} >{row[column]}</td>
+                    ))}
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default TreningJeden;
